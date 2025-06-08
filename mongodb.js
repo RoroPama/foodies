82f8b0b9b83b6faff6c4be78a63cc186349b52bf
Rolic PAMA <pamarolic017@gmail.com>
2025-06-08 15:28:05 +0100

    build issues fixed 4

--- mongodb.js
+++ mongodb.js
@@ -82,9 +82,9 @@ export async function connectToDatabase() {
   // ✅ Détection améliorée de l'environnement de build
   const isBuildTime =
     !uri ||
-    (process.env.NODE_ENV === "production" &&
-      (process.env.VERCEL_ENV === "build" ||
-        process.env.NEXT_PHASE === "phase-production-build"));
+    // process.env.NODE_ENV === "production" &&
+    process.env.VERCEL_ENV === "build" ||
+    process.env.NEXT_PHASE === "phase-production-build";
 
   if (isBuildTime) {
     console.log(
